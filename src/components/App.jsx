4cd15c7e8b2601fdf8abede60b88992d375f84d6
yaroslavkrymov <93751242+yaroslavkrymov@users.noreply.github.com>
2023-01-17 20:35:44 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  setInterval(updateTime, 1000);
+
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
+    </div>
+  );
+}
+
+//1. Given that you can get the current time using:
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
+
+export default App;
